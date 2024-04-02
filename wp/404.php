14b980c01b17aece5db23a310354aad34fe47380
halil_95 <halilaliyew@gmail.com>
2024-04-02 22:35:46 +0300

    create haml file

---
+++ wp/404.php
@@ -0,0 +1,5 @@
+<?php get_header();?>
+<main role="main">
+<p>this page not found</p>
+</main>
+<?php get_footer();?>
---
+++ wp/footer.php
@@ -0,0 +1,4 @@
+</body>
+<?php wp_footer();?>
+<script src="dist/script/script.js"></script>
+</html>
