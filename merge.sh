2a7e7fa0bfbef474bdec8b0b17f7277e8e700d07
halil_95 <halilaliyew@gmail.com>
2024-04-14 13:44:54 +0300

    готова

--- merge.sh
+++ merge.sh
@@ -6,6 +6,7 @@ HEAD_HTML="../html/header/head.html"
 
 # MAIN_HTML="../html/main/main.html"
     hero="../html/main/hero.html"
+    port="../html/main/port.html"
 
 FOOTER_HTML="../html/footer/footer.html"
 OUTPUT_HTML="index.html"
@@ -13,7 +14,8 @@ OUTPUT_HTML="index.html"
 
 
 # Используем команду cat для объединения содержимого HTML файлов
-cat "$HEAD_HTML" "$HEADER_HTML" "$hero" "$FOOTER_HTML" > "$OUTPUT_HTML"
+cat "$HEAD_HTML" "$HEADER_HTML" "$port" "$FOOTER_HTML" > "$OUTPUT_HTML"
+# cat "$HEAD_HTML" "$port" > "$OUTPUT_HTML"
 
 # Выводим сообщение об успешном завершении
 echo "HTML файлы успешно объединены в $OUTPUT_HTML"
